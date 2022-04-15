f2c07bc1962b1bc0cdfbc7f13d7ba48763d078e0
“Kaiyue0701” <kf2276@nyu.edu>
2022-04-15 15:29:08 -0400

    Add software

--- software/main.py
+++ software/main.py
@@ -2,15 +2,42 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+import reip
+
+import serial
+import numpy as np
+
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def demo():
+    dev = serial.Serial(port="/dev/cu.usbmodem0E22C4B41", baudrate=115200)
+    while True:
+        cmd = input()
+        if cmd == "exit":
+            break
+        dev.write(cmd.encode())
+        time.sleep(0.1)
+        raw = []
+        while dev.in_waiting:
+            raw.append(dev.read(1).decode())
+        msg = "".join(raw).strip()
+        # TODO: Parse two float values from msg
+        print(msg)
+    dev.close()
+
+
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # print_hi('PyCharm')
+    demo()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
